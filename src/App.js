f791926a9bd39edf7eb29dbec8c5d3ec5c782935
sajib-ahmed <onlineclass82@gmail.com>
2022-06-02 15:10:21 +0600

    route install

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import Orders from './components/Orders/Orders';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div>
           <Header> </Header>
-          <Shop> </Shop>
+          
+          <Routes>
+              <Route path='/' element={<Shop />}> </Route>
+              <Route path='/shop' element={<Shop />}> </Route>
+              <Route path='/orders' element={<Orders />}> </Route>
+              <Route path='/inventory' element={<Inventory />}> </Route>
+              <Route path='*' element={<NotFound />}> </Route>
+          </Routes>
+          
     </div>
   );
 }
